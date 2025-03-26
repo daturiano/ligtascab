59eb7a9077960beadeb5e4064b2bbdde0b1613d1
danturiano <dturiano.r@gmail.com>
2025-03-26 22:47:38 +0800

    fix(authentication): page padding

--- src/app/(authentication)/sign-in/page.tsx
+++ src/app/(authentication)/sign-in/page.tsx
@@ -4,14 +4,15 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import Seperator from "@/components/ui/seperator";
-import GoogleButton from "@/features/authentication/components/google-btn";
-import SignInForm from "@/features/authentication/components/sign-in-form";
+} from '@/components/ui/card';
+import Seperator from '@/components/ui/seperator';
+import GoogleButton from '@/features/authentication/components/google-btn';
+import SignInForm from '@/features/authentication/components/sign-in-form';
+import Link from 'next/link';
 
 export default function SignInPage() {
   return (
-    <Card className="w-full max-w-[28rem]">
+    <Card className="w-full max-w-[28rem] px-4">
       <CardHeader>
         <CardTitle className="text-2xl">Welcome back</CardTitle>
       </CardHeader>
@@ -25,9 +26,12 @@ export default function SignInPage() {
         <SignInForm />
       </CardContent>
       <CardFooter>
-        <p className="text-xs text-center lg:text-start lg:text-sm">
-          Need to create an account? Sign up
-        </p>
+        <div className="flex items-center gap-2">
+          <p className="text-xs lg:text-sm">Need to create an account?</p>
+          <Link href={'/sign-up'}>
+            <p className="text-blue-400 text-xs lg:text-sm">Sign up</p>
+          </Link>
+        </div>
       </CardFooter>
     </Card>
   );
--- src/app/(authentication)/sign-up/page.tsx
+++ src/app/(authentication)/sign-up/page.tsx
@@ -5,14 +5,14 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import Seperator from "@/components/ui/seperator";
-import GoogleButton from "@/features/authentication/components/google-btn";
-import SignUpForm from "@/features/authentication/components/sign-up-form";
+} from '@/components/ui/card';
+import Seperator from '@/components/ui/seperator';
+import GoogleButton from '@/features/authentication/components/google-btn';
+import SignUpForm from '@/features/authentication/components/sign-up-form';
 
 export default function SignUpPage() {
   return (
-    <Card className="max-w-[32rem]">
+    <Card className="max-w-[32rem] px-4">
       <CardHeader>
         <CardTitle className="text-2xl">Create operator account</CardTitle>
         <CardDescription className="lg:text-lg">
@@ -29,7 +29,7 @@ export default function SignUpPage() {
         <SignUpForm />
       </CardContent>
       <CardFooter>
-        <p className="text-xs text-center lg:text-start lg:text-sm">
+        <p className="text-xs text-center lg:text-sm">
           By creating your account, you agree to our Terms of Service , our
           Product T&C&apos;s and that you have read and understood our Privacy
           Policy.
