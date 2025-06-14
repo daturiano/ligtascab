a79d7a39e05c7f03b78befafaf993f93b5a15f65
danturiano <dturiano.r@gmail.com>
2025-06-14 13:03:07 +0800

    fix(drivers): not displaying image when drivers data length is 0

--- src/app/(private)/(drivers)/drivers/page.tsx
+++ src/app/(private)/(drivers)/drivers/page.tsx
@@ -160,7 +160,7 @@ export default function DriverPage() {
         </div>
       </div>
       <div className="min-w-full border-[0.3px] rounded-2xl max-h-[37rem] overflow-y-auto bg-card">
-        {!data ? (
+        {filteredDrivers.length <= 0 ? (
           <div className="flex items-center justify-center flex-col py-12">
             <Image src={emptyImage} alt="empty image" className="size-36" />
             <div className="flex flex-col space-y-4 text-center mb-8">
