5229548e251bbab9206779bd266140c3d9ef212e
danturiano <dturiano.r@gmail.com>
2025-06-22 15:24:51 +0800

    feat(tricycles): add qr code card on tricycle profile

--- src/app/(private)/(tricycles)/tricycles/[tricycleId]/page.tsx
+++ src/app/(private)/(tricycles)/tricycles/[tricycleId]/page.tsx
@@ -1,10 +1,12 @@
+import docuimg from '@/app/public/approve.png';
+import qrcode from '@/app/public/qr-code.png';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import ViewQRCode from '@/components/view-qr-code';
 import { fetchTricycleDetails } from '@/features/tricycles/actions/tricycles';
 import TricycleProfileCard from '@/features/tricycles/components/tricycle-profile-card';
 import ViewTricycleCompliance from '@/features/tricycles/components/view-tricycle-compliance';
 import { createClient } from '@/supabase/server';
-import { ArrowLeft, Files } from 'lucide-react';
-import docuimg from '@/app/public/approve.png';
+import { ArrowLeft } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -54,6 +56,26 @@ export default async function TricycleProfilePage({
                   <ViewTricycleCompliance
                     path={`${user.id}/tricycles/${tricycle.id}`}
                   />
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="w-full grow xl:grow-0 max-w-[500px] xl:max-w-[600px]">
+            <CardTitle className="px-6">Tricycle&apos;s QR Code</CardTitle>
+            <CardContent className="flex grow gap-4">
+              <div className="min-h-32 min-w-32 bg-gray-300 flex items-center justify-center rounded-md">
+                <Image src={qrcode} alt="license" width={60} height={60} />
+              </div>
+              <div className="w-full flex flex-col justify-between gap-4">
+                <p className="text-muted-foreground text-sm">
+                  Make sure to attach this QR Code to the correct tricycle.
+                </p>
+                <div className="flex flex-row gap-2">
+                  <ViewQRCode id={tricycle.id}>
+                    <div className="w-full bg-primary text-white p-2 text-sm rounded-md cursor-pointer">
+                      View QR Code
+                    </div>
+                  </ViewQRCode>
                 </div>
               </div>
             </CardContent>
