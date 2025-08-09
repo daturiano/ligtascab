2dfae9bad939ef03f461e25317027e0ebfc591d5
danturiano <dturiano.r@gmail.com>
2025-08-09 12:56:37 +0800

    fix(home): add logout button on user profile

--- src/components/private/user-profile.tsx
+++ src/components/private/user-profile.tsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { useQuery } from '@tanstack/react-query';
-import { getOperator } from '@/db/db';
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useQuery } from "@tanstack/react-query";
+import { getOperator } from "@/db/db";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { signOut } from "@/features/authentication/actions/authentication";
 
 export default function UserProfile() {
   const { data: operator } = useQuery({
-    queryKey: ['operator'],
+    queryKey: ["operator"],
     queryFn: getOperator,
   });
 
   if (!operator) return null;
   return (
-    <Avatar className="size-10 rounded-full bg-muted-foreground/20 flex items-center justify-center">
-      <AvatarImage
-        src={operator.image ?? undefined}
-        alt={operator.first_name}
-      />
-      <AvatarFallback className="size-10 rounded-full bg-muted-foreground/10 flex items-center justify-center">
-        <p className="font-medium">
-          {operator.first_name.charAt(0).toUpperCase()}
-          {operator.last_name.charAt(0).toUpperCase()}
-        </p>
-      </AvatarFallback>
-    </Avatar>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar className="bg-muted-foreground/20 flex size-10 items-center justify-center rounded-full">
+          <AvatarImage
+            src={operator.image ?? undefined}
+            alt={operator.first_name}
+          />
+          <AvatarFallback className="bg-muted-foreground/10 flex size-10 items-center justify-center rounded-full">
+            <p className="font-medium">
+              {operator.first_name.charAt(0).toUpperCase()}
+              {operator.last_name.charAt(0).toUpperCase()}
+            </p>
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
